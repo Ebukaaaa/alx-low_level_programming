c0c8c6c8a649a6fc269c6a10190e8f9a54a2848e
Ebukaaaa <ebukauwaezuoke1420@gmail.com>
2023-09-21 01:33:44 -0700

    0-strcat.C

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,26 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will append
- *@src: string that will be concatenated upon
- *
- * Return: returns pointer to @dest
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
  */
-
-/*
-1. First, we’re declaring a function called _strcat that takes two arguments: dest and src.
-2.  Next, we’re declaring two variables: index and dest_len.
-3.  Then, we’re using a while loop to iterate through the dest string and find its length.
-4.  Next, we’re using a for loop to iterate through the src string and append each character to the end of the dest string.
-5.  Finally, we’re returning the dest string.
-*/
-/*
-Time Complexity: O(n)
-*/
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int lengthD, lengthS;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	lengthD = 0;
+	lengthS = 0;
 
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthD < 97)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
 	return (dest);
 }
