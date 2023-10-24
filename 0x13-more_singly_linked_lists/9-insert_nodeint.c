598d48abc75a006aad4124726c8194d30222a300
Ebukaaaa <ebukauwaezuoke1420@gmail.com>
2023-10-24 10:30:50 -0700

    ebuka

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,3 +1,4 @@
+#include "lists.h"
 
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list,
@@ -32,12 +33,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 	if (i == idx - 1)
 	{
-		new->next = temp->next;
-		temp->next = new;
-		return (new);
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
 	}
 	else
-		temp = temp->next;
+	temp = temp->next;
 	}
 
 	return (NULL);
